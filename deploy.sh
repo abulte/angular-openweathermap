705a5f6cc2d0c8c9d1e001d948659e2306feea69
Alexandre Bulté <alexandre@bulte.net>
2014-04-05 01:00:57 +0200

    correct locale inclusion

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,8 @@ grunt build
 cd ../webapp_hosted
 git checkout gh-pages
 rm -rf *
+echo "meteo.bulte.net" > CNAME
 cp -a ../webapp/dist/* .
-git add .
+git add --all .
 git commit -a -m 'Deployed from local copy'
 git push
