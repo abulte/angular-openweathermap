db21240aaa3f57a3ad729b03399f4bbb5a94c64e
Alexandre Bulté <alexandre@bulte.net>
2014-04-05 19:52:42 +0200

    freezing alert

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,10 +1,37 @@
 'use strict';
 
+var showNotifCallback = function(temp, NotifObj) {
+  new NotifObj('Il va geler !', {'body': 'Il est prévu ' + temp + '° cette nuit.'});
+  // n.onclick = function () {
+  //   this.close();
+  // };
+};
+
 angular.module('meteoApp')
 
-.controller('MainCtrl', function($scope, $rootScope, DailyForecast) {
+.controller('MainCtrl', function($scope, $rootScope, DailyForecast, $window) {
   $rootScope.jourLoading = 1;
-  $scope.dailyForecast = DailyForecast.query({}, function() {
+  $scope.dailyForecast = DailyForecast.query({}, function(data) {
+
+    // freezing alert - put that elsewhere
+    var tempNight = data[0].temp.night;
+    if (parseFloat(tempNight) < 5) {
+      if ('Notification' in $window) {
+        var perm = $window.Notification.permission;
+        if (perm === 'default') {
+          $window.Notification.requestPermission(function(perm) {
+            if(perm === 'granted') {
+              showNotifCallback(tempNight, $window.Notification);
+            }
+          });
+        } else if (perm === 'granted') {
+          showNotifCallback(tempNight, $window.Notification);
+        }
+      } else {
+        console.log('No notification API support.');
+      }
+    }
+
     $rootScope.jourLoading = 0;
   }, function(error) {
     $scope.errorMsg = error;
